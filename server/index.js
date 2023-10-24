5bee6a898b59a221a2a543f44b38172945dee337
unknown <godhustler90956@gmail.com>
2023-10-24 12:21:30 +0530

    update2

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import "express-async-errors";
 import helmet from "helmet";
 import xss from "xss-clean";
@@ -60,16 +61,8 @@ app.use("/api/admin", uploadRoute);
 // // //* HTTP GET Request
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-  res.send("Home");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
 });
+
 
 //* Middlewares
 app.use(notFoundMiddleware);
