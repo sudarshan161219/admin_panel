24e14db0678491da71e18b762def2a86eb5d3140
unknown <godhustler90956@gmail.com>
2023-11-02 23:02:10 +0530

    11:02pm

--- client/src/adminpanel/admin/userInfo/UserInfo.jsx
+++ client/src/adminpanel/admin/userInfo/UserInfo.jsx
@@ -10,7 +10,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { Item } from "../../../components/export"
-import { ListItemSecondaryAction } from '@mui/material';
+
 const UserInfo = () => {
 
     const [user, setUser] = useState([]);
